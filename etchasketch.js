5bf39058a1d08af6745dc4a270251f41d1cbee37
bmd7700 <bendrt@gmail.com>
2025-02-26 12:43:01 -0500

    add css for index.html

--- etchasketch.js
+++ etchasketch.js
@@ -1,3 +1,13 @@
 /**
- * Create a 16x16 grid of square divs using Javascript. 
- */
+ * 1. [ ] Create a 16x16 grid of square divs using Javascript. 
+ *      They should nest into the container div in the HTML file. 
+ * 2. [ ] Set up a Hover effect so that the grid divs change color when the mouse passes
+ *      over them. 
+ * 3. [ ] Set up a button that will generate a popup asking users to select a new grid size
+ *      Once clicked, the program will replace the 16x16 grid with a grid of user-specified size.
+ *      3a. [ ] Upper limit for user-generated grid is 100 x 100. 
+ * 
+ */ 
+
+console.log('JS found!');
+
