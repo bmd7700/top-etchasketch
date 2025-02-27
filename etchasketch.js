ded5cc2caa6237ff8edabf6530e46e2518f9f0c4
bmd7700 <bendrt@gmail.com>
2025-02-27 14:53:15 -0500

    add modifyRules to dynamically change CSS

--- etchasketch.js
+++ etchasketch.js
@@ -4,6 +4,7 @@
  * 2. [ ] Set up a Hover effect so that the grid divs change color when the mouse passes
  *      over them. 
  * 3. [ ] Set up a button that will generate a popup asking users to select a new grid size
+ *      Note: this should set number of squares PER SIDE of the grid. 
  *      Once clicked, the program will replace the 16x16 grid with a grid of user-specified size.
  *      3a. [ ] Upper limit for user-generated grid is 100 x 100. 
  * 
@@ -17,23 +18,44 @@ for(i = 0; i<16; i++){
     console.log('appending');
 }
 
-//manipulate CSS to get container cells into grid
-
 //called by onClick from button id "userGrid"
 function setUserGrid(){
     while(container.firstChild){
         container.removeChild(container.firstChild);
         console.log('removing');
     }
+
     let gridSize = prompt('Enter a number between 1 and 100: ');
+    const gridFactor = 960 / Math.floor(Math.sqrt(gridSize));
+    console.log("grid factor: " + gridFactor.toString() + "px");
+
     if(gridSize > 0 && gridSize <101){
         for(let i = 0; i < gridSize; i++){
-            appendDiv();
-        }
-    } else {alert("That is not a number between 1 and 100, you lump!")}
+            appendDiv("userStyle");
+            console.log('appending');
+        };
+        modifyRules(gridFactor);
+    } else {
+        alert("That is not a number between 1 and 100, you lump!")
+    }
 }
 
 function appendDiv(className){
     container.appendChild(document.createElement("div")).className = className.toString();
     console.log('appendDiv className: ' + className);
+}
+
+function modifyRules(gridFactor){
+    const styleSheet = document.styleSheets[0];
+    let elementRule;
+    let size = gridFactor.toString() + 'px';
+    console.log('size: ' + size);
+    for(let i = 0; i < styleSheet.cssRules.length; i++){
+        if(styleSheet.cssRules[i].selectorText === '.userStyle'){
+            elementRule = styleSheet.cssRules[i];
+            elementRule.style.setProperty("height", size);
+            elementRule.style.setProperty("width", size);
+            console.log(styleSheet)
+        }
+    }
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 This project is from The Odin Project.
 Instructions and reference can be found at:
 https://www.theodinproject.com/lessons/foundations-etch-a-sketch
+
+
+container width: 960px;
